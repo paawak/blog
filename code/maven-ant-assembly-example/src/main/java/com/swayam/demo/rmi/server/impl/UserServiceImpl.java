ba7a901152e17ddc5dccad97eff93eeaa9947a0b
Palash Ray <paawak@gmail.com>
2014-07-27 10:48:18 -0400

    new home for maven-ant-example

---
+++ code/maven-ant-assembly-example/src/main/java/com/swayam/demo/rmi/server/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.swayam.demo.rmi.server.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.swayam.demo.rmi.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<String> getCurrentUsers() {
+        return Arrays.asList("AAA", "BBB", "CCC", "DDD");
+    }
+}
