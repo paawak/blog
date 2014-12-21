8227d8c88afeb9bd72b0d536a0950a06dd1e2209
paawak <paawak@gmail.com>
2014-12-20 23:47:54 -0500

    treetable

---
+++ code/lambda-demo/src/main/java/com/swayam/demo/lambda/BankDetailTreeTableModel.java
@@ -0,0 +1,111 @@
+package com.swayam.demo.lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.jdesktop.swingx.treetable.AbstractTreeTableModel;
+
+public class BankDetailTreeTableModel extends AbstractTreeTableModel {
+
+	private static final int COULMN_COUNT = 19;
+
+	private static final String[] COLUMN_NAMES = new String[] { "id", "age",
+			"job", "marital", "education", "default", "balance", "housing",
+			"loan", "contact", "day", "month", "duration", "campaign", "pdays",
+			"previous", "poutcome", "y" };
+
+	private static final String ROOT = "_ROOT_";
+
+	private final Map<String, List<BankDetail>> groupedBankDetails;
+	private final List<String> groups;
+
+	public BankDetailTreeTableModel(
+			Map<String, List<BankDetail>> groupedBankDetails) {
+		super(ROOT);
+		this.groupedBankDetails = groupedBankDetails;
+		groups = getGroups(groupedBankDetails);
+	}
+
+	@Override
+	public int getColumnCount() {
+		return COULMN_COUNT;
+	}
+
+	@Override
+	public Object getValueAt(Object node, int column) {
+		if (node instanceof String) {
+			if (column == 0) {
+				return node;
+			}
+			return COLUMN_NAMES[column - 1];
+		} else if (node instanceof BankDetail) {
+			if (column == 0) {
+				return null;
+			}
+			BankDetail bankDetail = (BankDetail) node;
+			String[] row = createTableRow(bankDetail);
+			return row[column - 1];
+		}
+
+		return null;
+	}
+
+	@Override
+	public Object getChild(Object parent, int index) {
+		if (ROOT.equals(parent)) {
+			return groups.get(index);
+		} else if (parent instanceof String) {
+			return groupedBankDetails.get(parent).get(index);
+		}
+
+		return null;
+	}
+
+	@Override
+	public int getChildCount(Object parent) {
+		if (ROOT.equals(parent)) {
+			return groups.size();
+		} else if (parent instanceof String) {
+			return groupedBankDetails.get(parent).size();
+		}
+		return 0;
+	}
+
+	@Override
+	public int getIndexOfChild(Object parent, Object child) {
+		return 0;
+	}
+
+	private List<String> getGroups(
+			Map<String, List<BankDetail>> groupedBankDetails) {
+		List<String> groups = new ArrayList<>(groupedBankDetails.keySet());
+		Collections.sort(groups);
+		return groups;
+	}
+
+	private String[] createTableRow(BankDetail bankDetail) {
+		String[] row = new String[18];
+		row[0] = Integer.toString(bankDetail.getId());
+		row[1] = Integer.toString(bankDetail.getAge());
+		row[2] = bankDetail.getJob();
+		row[3] = bankDetail.getMarital();
+		row[4] = bankDetail.getEducation();
+		row[5] = bankDetail.getDefaulted();
+		row[6] = bankDetail.getBalance().toPlainString();
+		row[7] = bankDetail.getHousing();
+		row[8] = bankDetail.getLoan();
+		row[9] = bankDetail.getContact();
+		row[10] = Integer.toString(bankDetail.getDay());
+		row[11] = bankDetail.getMonth();
+		row[12] = Integer.toString(bankDetail.getDuration());
+		row[13] = Integer.toString(bankDetail.getCampaign());
+		row[14] = Integer.toString(bankDetail.getPdays());
+		row[15] = Integer.toString(bankDetail.getPrevious());
+		row[16] = bankDetail.getPoutcome();
+		row[17] = bankDetail.getY();
+		return row;
+	}
+
+}
--- code/lambda-demo/src/main/java/com/swayam/demo/lambda/GroupingDemoFrame.java
+++ code/lambda-demo/src/main/java/com/swayam/demo/lambda/GroupingDemoFrame.java
@@ -8,6 +8,9 @@ import java.util.Map;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+import org.jdesktop.swingx.JXTreeTable;
+import org.jdesktop.swingx.treetable.TreeTableModel;
+
 /**
  *
  * @author paawak
@@ -43,9 +46,10 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
 			Map<String, List<BankDetail>> groupedBankDetails = bankDetailService
 					.getBankDetails(selectedGroup);
 
-			TableModel tableModel = createTableModel(groupedBankDetails);
+			// TableModel tableModel = createTableModel(groupedBankDetails);
 
-			tblBankDetails.setModel(tableModel);
+			tblBankDetails.setTreeTableModel(new BankDetailTreeTableModel(
+					groupedBankDetails));
 
 		});
 
@@ -53,6 +57,8 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
 
 	private TableModel createTableModel(
 			Map<String, List<BankDetail>> groupedBankDetails) {
+
+		TreeTableModel treeTableModel;
 
 		List<String[]> tableData = new ArrayList<>();
 
@@ -103,107 +109,146 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
 	// <editor-fold defaultstate="collapsed"
 	// <editor-fold defaultstate="collapsed"
 	// <editor-fold defaultstate="collapsed"
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+	// <editor-fold defaultstate="collapsed"
+	// desc="Generated Code">//GEN-BEGIN:initComponents
+	private void initComponents() {
 
-        btGrpBankDetails = new javax.swing.ButtonGroup();
-        jLabel1 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
-        rdBtJob = new javax.swing.JRadioButton();
-        rdBtMaritalStatus = new javax.swing.JRadioButton();
-        rdBtEducation = new javax.swing.JRadioButton();
-        btSubmit = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblBankDetails = new javax.swing.JTable();
+		btGrpBankDetails = new javax.swing.ButtonGroup();
+		jLabel1 = new javax.swing.JLabel();
+		jPanel1 = new javax.swing.JPanel();
+		rdBtJob = new javax.swing.JRadioButton();
+		rdBtMaritalStatus = new javax.swing.JRadioButton();
+		rdBtEducation = new javax.swing.JRadioButton();
+		btSubmit = new javax.swing.JButton();
+		jScrollPane1 = new javax.swing.JScrollPane();
+		tblBankDetails = new JXTreeTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Grouping Demo");
-        setPreferredSize(new java.awt.Dimension(800, 550));
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Grouping Demo");
+		setPreferredSize(new java.awt.Dimension(800, 550));
 
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Grouping Demo");
+		jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		jLabel1.setText("Grouping Demo");
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Group By"));
+		jPanel1.setBorder(javax.swing.BorderFactory
+				.createTitledBorder("Group By"));
 
-        btGrpBankDetails.add(rdBtJob);
-        rdBtJob.setSelected(true);
-        rdBtJob.setText("Job");
+		btGrpBankDetails.add(rdBtJob);
+		rdBtJob.setSelected(true);
+		rdBtJob.setText("Job");
 
-        btGrpBankDetails.add(rdBtMaritalStatus);
-        rdBtMaritalStatus.setText("Marital Status");
+		btGrpBankDetails.add(rdBtMaritalStatus);
+		rdBtMaritalStatus.setText("Marital Status");
 
-        btGrpBankDetails.add(rdBtEducation);
-        rdBtEducation.setText("Education");
+		btGrpBankDetails.add(rdBtEducation);
+		rdBtEducation.setText("Education");
 
-        btSubmit.setText("Submit");
+		btSubmit.setText("Submit");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(rdBtJob)
-                            .addComponent(rdBtMaritalStatus)
-                            .addComponent(rdBtEducation)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(42, 42, 42)
-                        .addComponent(btSubmit)))
-                .addContainerGap(11, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(rdBtJob)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rdBtMaritalStatus)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rdBtEducation)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
-                .addComponent(btSubmit)
-                .addContainerGap())
-        );
+		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(
+				jPanel1);
+		jPanel1.setLayout(jPanel1Layout);
+		jPanel1Layout
+				.setHorizontalGroup(jPanel1Layout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								jPanel1Layout
+										.createSequentialGroup()
+										.addGroup(
+												jPanel1Layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.LEADING)
+														.addGroup(
+																jPanel1Layout
+																		.createSequentialGroup()
+																		.addContainerGap()
+																		.addGroup(
+																				jPanel1Layout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.LEADING)
+																						.addComponent(
+																								rdBtJob)
+																						.addComponent(
+																								rdBtMaritalStatus)
+																						.addComponent(
+																								rdBtEducation)))
+														.addGroup(
+																jPanel1Layout
+																		.createSequentialGroup()
+																		.addGap(42,
+																				42,
+																				42)
+																		.addComponent(
+																				btSubmit)))
+										.addContainerGap(11, Short.MAX_VALUE)));
+		jPanel1Layout
+				.setVerticalGroup(jPanel1Layout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								jPanel1Layout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addComponent(rdBtJob)
+										.addPreferredGap(
+												javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+										.addComponent(rdBtMaritalStatus)
+										.addPreferredGap(
+												javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+										.addComponent(rdBtEducation)
+										.addPreferredGap(
+												javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+												9, Short.MAX_VALUE)
+										.addComponent(btSubmit)
+										.addContainerGap()));
 
-        tblBankDetails.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
+		// tblBankDetails.setModel(new javax.swing.table.DefaultTableModel(
+		// new Object[][] {
+		//
+		// }, new String[] {
+		//
+		// }));
+		jScrollPane1.setViewportView(tblBankDetails);
 
-            },
-            new String [] {
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+				getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout
+				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+						javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+				.addGroup(
+						layout.createSequentialGroup()
+								.addComponent(jPanel1,
+										javax.swing.GroupLayout.PREFERRED_SIZE,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										javax.swing.GroupLayout.PREFERRED_SIZE)
+								.addGap(0, 0, Short.MAX_VALUE))
+				.addGroup(
+						layout.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(jScrollPane1,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										709, Short.MAX_VALUE).addContainerGap()));
+		layout.setVerticalGroup(layout
+				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(
+						layout.createSequentialGroup()
+								.addComponent(jLabel1)
+								.addPreferredGap(
+										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(jPanel1,
+										javax.swing.GroupLayout.PREFERRED_SIZE,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										javax.swing.GroupLayout.PREFERRED_SIZE)
+								.addGap(18, 18, 18)
+								.addComponent(jScrollPane1,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										249, Short.MAX_VALUE).addContainerGap()));
 
-            }
-        ));
-        jScrollPane1.setViewportView(tblBankDetails);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 709, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+		pack();
+	}// </editor-fold>//GEN-END:initComponents
 
 	/**
 	 * @param args
@@ -247,22 +292,20 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
 		// </editor-fold>
 
 		/* Create and display the form */
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				new GroupingDemoFrame().setVisible(true);
-			}
+		java.awt.EventQueue.invokeLater(() -> {
+			new GroupingDemoFrame().setVisible(true);
 		});
 	}
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.ButtonGroup btGrpBankDetails;
-    private javax.swing.JButton btSubmit;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JRadioButton rdBtEducation;
-    private javax.swing.JRadioButton rdBtJob;
-    private javax.swing.JRadioButton rdBtMaritalStatus;
-    private javax.swing.JTable tblBankDetails;
-    // End of variables declaration//GEN-END:variables
+	// Variables declaration - do not modify//GEN-BEGIN:variables
+	private javax.swing.ButtonGroup btGrpBankDetails;
+	private javax.swing.JButton btSubmit;
+	private javax.swing.JLabel jLabel1;
+	private javax.swing.JPanel jPanel1;
+	private javax.swing.JScrollPane jScrollPane1;
+	private javax.swing.JRadioButton rdBtEducation;
+	private javax.swing.JRadioButton rdBtJob;
+	private javax.swing.JRadioButton rdBtMaritalStatus;
+	private JXTreeTable tblBankDetails;
+	// End of variables declaration//GEN-END:variables
 }
