008f41c35e8114d136aa08bec83df4f93f2c669f
paawak <paawak@gmail.com>
2015-01-18 18:13:14 -0500

    dynamic code download eample

---
+++ code/jini/unsecure/plain/bare-bones-rmi-server/src/main/java/com/swayam/demo/rmi/server/core/SimpleNonSecureRmiServerWithDynamicCodeDownload.java
@@ -0,0 +1,57 @@
+package com.swayam.demo.rmi.server.core;
+
+import java.rmi.Remote;
+
+import net.jini.core.discovery.LookupLocator;
+import net.jini.core.entry.Entry;
+import net.jini.core.lookup.ServiceID;
+import net.jini.discovery.DiscoveryManagement;
+import net.jini.discovery.LookupLocatorDiscovery;
+import net.jini.export.Exporter;
+import net.jini.jrmp.JrmpExporter;
+import net.jini.lease.LeaseRenewalManager;
+import net.jini.lookup.JoinManager;
+import net.jini.lookup.entry.Name;
+
+import com.swayam.demo.rmi.server.impl.BankAccountServiceImpl;
+import com.swayam.demo.rmi.server.impl.UserServiceImpl;
+import com.swayam.demo.rmi.service.BankAccountService;
+import com.swayam.demo.rmi.service.UserService;
+
+public class SimpleNonSecureRmiServerWithDynamicCodeDownload {
+
+    private final DiscoveryManagement discoveryManager;
+    private final LeaseRenewalManager leaseRenewalManager;
+
+    public SimpleNonSecureRmiServerWithDynamicCodeDownload() throws Exception {
+        discoveryManager = new LookupLocatorDiscovery(new LookupLocator[] { new LookupLocator("jini://localhost:4160") });
+        leaseRenewalManager = new LeaseRenewalManager();
+    }
+
+    private void exportAndJoinServices() throws Exception {
+        exportAndJoinService(new UserServiceImpl(), discoveryManager, leaseRenewalManager, UserService.class.getSimpleName());
+        exportAndJoinService(new BankAccountServiceImpl(), discoveryManager, leaseRenewalManager, BankAccountService.class.getSimpleName());
+    }
+
+    private void exportAndJoinService(Remote service, DiscoveryManagement discoveryManager, LeaseRenewalManager leaseRenewalManager, String serviceName) throws Exception {
+        // the exporter can export only 1 remote service
+        // this is VERY important: prevents the class from getting GC-ed
+        Exporter exporter = getExporter();
+        Remote exportedService = exporter.export(service);
+        JoinManager joinManager = new JoinManager(exportedService, new Entry[] { new Name(serviceName) }, (ServiceID) null, discoveryManager, leaseRenewalManager);
+    }
+
+    private Exporter getExporter() {
+        return new JrmpExporter();
+        // return new BasicJeriExporter(TcpServerEndpoint.getInstance(0), new
+        // BasicILFactory());
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("java.rmi.server.codebase", "http://localhost:8080/RmiService-1.0.jar");
+        SimpleNonSecureRmiServerWithDynamicCodeDownload server = new SimpleNonSecureRmiServerWithDynamicCodeDownload();
+        server.exportAndJoinServices();
+        System.out.println("Hello server is ready");
+    }
+
+}
--- code/jini/unsecure/plain/rmi-client-dynamic-code-download/src/main/java/com/swayam/demo/rmi/client/DynamicDownloadRmiClient.java
+++ code/jini/unsecure/plain/rmi-client-dynamic-code-download/src/main/java/com/swayam/demo/rmi/client/DynamicDownloadRmiClient.java
@@ -1,7 +1,6 @@
 package com.swayam.demo.rmi.client;
 
 import java.lang.reflect.Method;
-import java.rmi.RMISecurityManager;
 import java.rmi.Remote;
 
 import net.jini.core.discovery.LookupLocator;
@@ -10,37 +9,43 @@ import net.jini.core.lookup.ServiceRegistrar;
 import net.jini.core.lookup.ServiceTemplate;
 import net.jini.lookup.entry.Name;
 
-
-
 public class DynamicDownloadRmiClient {
 
-	/**
-	 * Needs to be run with the following JVM args:<br/>
-	 * <ul>
-	 * <li>-Djava.security.policy=/full-path/RmiClient/src/main/resources/policy.all</li>
-	 * <li>-Djava.rmi.server.RMIClassLoaderSpi=net.jini.loader.pref.PreferredClassProvider</li>
-	 * <li>-Djava.rmi.server.codebase=http://localhost:8080/RmiService-1.0.jar</li>
-	 * </ul>
-	 */
-	public static void main(String[] args) throws Exception {
-		
-		String codeBase = System.getProperty("java.rmi.server.codebase");
-		System.out.println("DynamicDownloadRmiClient.main() codeBase=" + codeBase);
-		
-		System.setSecurityManager(new RMISecurityManager());
-		LookupLocator lookup = new LookupLocator("jini://localhost:4160");
+    /**
+     * Needs to be run with the following JVM args:<br/>
+     * <ul>
+     * <li>
+     * -Djava.security.policy=/full-path/RmiClient/src/main/resources/policy.all
+     * </li>
+     * <li>-Djava.rmi.server.RMIClassLoaderSpi=net.jini.loader.pref.
+     * PreferredClassProvider</li>
+     * <li>-Djava.rmi.server.codebase=http://localhost:8080/RmiService-1.0.jar</li>
+     * </ul>
+     */
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("java.security.policy", DynamicDownloadRmiClient.class.getResource("/policy.all").getPath());
+        // System.setProperty(RMIClassLoaderSpi.class.getName(),
+        // MyClassProvider.class.getName());
+
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+            System.out.println("DynamicDownloadRmiClient.main() setting custom security manager");
+        }
+
+        LookupLocator lookup = new LookupLocator("jini://localhost:4160");
 
         ServiceRegistrar registrar = lookup.getRegistrar();
-        
-        //give the poor guys some time to lookup
-		Thread.sleep(1000); 
 
-		Remote userService = (Remote) registrar.lookup(new ServiceTemplate(null,null, new Entry[] { new Name("UserService") }));
+        // give the poor guys some time to lookup
+        Thread.sleep(1000);
 
-		Method getCurrentUsers = userService.getClass().getDeclaredMethod("getCurrentUsers");
-		Object result = getCurrentUsers.invoke(userService);
-		System.out.println("Getting current users from remote: " + result);
+        Remote userService = (Remote) registrar.lookup(new ServiceTemplate(null, null, new Entry[] { new Name("UserService") }));
 
-	}
+        Method getCurrentUsers = userService.getClass().getDeclaredMethod("getCurrentUsers");
+        Object result = getCurrentUsers.invoke(userService);
+        System.out.println("Getting current users from remote: " + result);
+
+    }
 
 }
