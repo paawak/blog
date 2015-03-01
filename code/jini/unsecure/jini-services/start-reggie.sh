db150d999e3372364c0c43f66b4b72e454fb13e4
paawak <paawak@gmail.com>
2015-02-28 21:10:04 -0500

    add logging properties

--- code/jini/unsecure/jini-services/start-reggie.sh
+++ code/jini/unsecure/jini-services/start-reggie.sh
@@ -1,1 +1,1 @@
-java -Djava.ext.dirs=reggie-libs -jar reggie-libs/start.jar config/start-reggie.config
+java -Djava.ext.dirs=reggie-libs -Djava.util.logging.config.file=config/logging.properties -jar reggie-libs/start.jar config/start-reggie.config
