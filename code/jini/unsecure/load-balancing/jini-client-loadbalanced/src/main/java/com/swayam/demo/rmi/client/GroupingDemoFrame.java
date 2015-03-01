4b874e00544843293dce8d15b43d12c4ba43157c
paawak <paawak@gmail.com>
2015-02-28 23:57:20 -0500

    a

--- code/jini/unsecure/load-balancing/jini-client-loadbalanced/src/main/java/com/swayam/demo/rmi/client/GroupingDemoFrame.java
+++ code/jini/unsecure/load-balancing/jini-client-loadbalanced/src/main/java/com/swayam/demo/rmi/client/GroupingDemoFrame.java
@@ -23,6 +23,7 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
      */
     public GroupingDemoFrame(BankDetailService bankDetailService) {
         this.bankDetailService = bankDetailService;
+
         initComponents();
         attachListeners();
     }
@@ -64,20 +65,17 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed"
     // <editor-fold defaultstate="collapsed"
     // <editor-fold defaultstate="collapsed"
+    // <editor-fold defaultstate="collapsed"
     // desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         btGrpBankDetails = new javax.swing.ButtonGroup();
-        btGrpFeatureToUse = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         rdBtJob = new javax.swing.JRadioButton();
         rdBtMaritalStatus = new javax.swing.JRadioButton();
         rdBtEducation = new javax.swing.JRadioButton();
         btSubmit = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
-        rdBtFeatureJdk8 = new javax.swing.JRadioButton();
-        rdBtFeaturePreJdk8 = new javax.swing.JRadioButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         treeTblBankDetails = new org.jdesktop.swingx.JXTreeTable();
 
@@ -88,8 +86,7 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Grouping Demo");
 
-        jPanel1.setBorder(javax.swing.BorderFactory
-                .createTitledBorder("Group By"));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Group By"));
 
         btGrpBankDetails.add(rdBtJob);
         rdBtJob.setSelected(true);
@@ -103,20 +100,17 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
 
         btSubmit.setText("Submit");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(
-                jPanel1);
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout
                 .setHorizontalGroup(jPanel1Layout
-                        .createParallelGroup(
-                                javax.swing.GroupLayout.Alignment.LEADING)
+                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(
                                 jPanel1Layout
                                         .createSequentialGroup()
                                         .addGroup(
                                                 jPanel1Layout
-                                                        .createParallelGroup(
-                                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addGroup(
                                                                 jPanel1Layout
                                                                         .createSequentialGroup()
@@ -125,145 +119,102 @@ public class GroupingDemoFrame extends javax.swing.JFrame {
                                                                                 jPanel1Layout
                                                                                         .createParallelGroup(
                                                                                                 javax.swing.GroupLayout.Alignment.LEADING)
-                                                                                        .addComponent(
-                                                                                                rdBtJob)
-                                                                                        .addComponent(
-                                                                                                rdBtMaritalStatus)
-                                                                                        .addComponent(
-                                                                                                rdBtEducation)))
+                                                                                        .addComponent(rdBtJob)
+                                                                                        .addComponent(rdBtMaritalStatus)
+                                                                                        .addComponent(rdBtEducation)))
                                                         .addGroup(
-                                                                jPanel1Layout
-                                                                        .createSequentialGroup()
-                                                                        .addGap(42,
-                                                                                42,
-                                                                                42)
-                                                                        .addComponent(
-                                                                                btSubmit)))
+                                                                jPanel1Layout.createSequentialGroup()
+                                                                        .addGap(42, 42, 42).addComponent(btSubmit)))
                                         .addContainerGap(11, Short.MAX_VALUE)));
         jPanel1Layout
-                .setVerticalGroup(jPanel1Layout
-                        .createParallelGroup(
-                                javax.swing.GroupLayout.Alignment.LEADING)
+                .setVerticalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(
                                 jPanel1Layout
                                         .createSequentialGroup()
                                         .addContainerGap()
                                         .addComponent(rdBtJob)
-                                        .addPreferredGap(
-                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(rdBtMaritalStatus)
-                                        .addPreferredGap(
-                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(rdBtEducation)
-                                        .addPreferredGap(
-                                                javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-                                                9, Short.MAX_VALUE)
-                                        .addComponent(btSubmit)
-                                        .addContainerGap()));
-
-        jPanel2.setBorder(javax.swing.BorderFactory
-                .createTitledBorder("Feature to use for group by"));
-
-        btGrpFeatureToUse.add(rdBtFeatureJdk8);
-        rdBtFeatureJdk8.setSelected(true);
-        rdBtFeatureJdk8.setText("JDK 8");
-
-        btGrpFeatureToUse.add(rdBtFeaturePreJdk8);
-        rdBtFeaturePreJdk8.setText("Pre JDK 8");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(
-                jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout
-                .setHorizontalGroup(jPanel2Layout
-                        .createParallelGroup(
-                                javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(
-                                jPanel2Layout
-                                        .createSequentialGroup()
-                                        .addContainerGap()
-                                        .addGroup(
-                                                jPanel2Layout
-                                                        .createParallelGroup(
-                                                                javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addComponent(
-                                                                rdBtFeaturePreJdk8)
-                                                        .addComponent(
-                                                                rdBtFeatureJdk8))
-                                        .addContainerGap(122, Short.MAX_VALUE)));
-        jPanel2Layout.setVerticalGroup(jPanel2Layout.createParallelGroup(
-                javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-                jPanel2Layout
-                        .createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE,
-                                Short.MAX_VALUE).addComponent(rdBtFeatureJdk8)
-                        .addGap(18, 18, 18).addComponent(rdBtFeaturePreJdk8)
-                        .addGap(42, 42, 42)));
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9,
+                                                Short.MAX_VALUE).addComponent(btSubmit).addContainerGap()));
 
         jScrollPane2.setViewportView(treeTblBankDetails);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
-                getContentPane());
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(layout
                 .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE,
-                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,
+                        Short.MAX_VALUE)
                 .addGroup(
                         layout.createSequentialGroup()
-                                .addComponent(jPanel1,
-                                        javax.swing.GroupLayout.PREFERRED_SIZE,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                        javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(
-                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jPanel2,
-                                        javax.swing.GroupLayout.PREFERRED_SIZE,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                        javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 344, Short.MAX_VALUE))
-                .addComponent(jScrollPane2));
-        layout.setVerticalGroup(layout
-                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(
-                        layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(
-                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(
-                                        layout.createParallelGroup(
-                                                javax.swing.GroupLayout.Alignment.LEADING,
-                                                false)
-                                                .addComponent(
-                                                        jPanel1,
-                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                        Short.MAX_VALUE)
-                                                .addComponent(
-                                                        jPanel2,
-                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                        Short.MAX_VALUE))
-                                .addPreferredGap(
-                                        javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jScrollPane2,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                        267, Short.MAX_VALUE)));
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 578, Short.MAX_VALUE)).addComponent(jScrollPane2));
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(
+                layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    // /**
+    // * @param args
+    // * the command line arguments
+    // */
+    // public static void main(String args[]) {
+    // /* Set the Nimbus look and feel */
+    // // <editor-fold defaultstate="collapsed"
+    // // desc=" Look and feel setting code (optional) ">
+    // /*
+    // * If Nimbus (introduced in Java SE 6) is not available, stay with the
+    // * default look and feel. For details see
+    // * http://download.oracle.com/javase
+    // * /tutorial/uiswing/lookandfeel/plaf.html
+    // */
+    // try {
+    // for (javax.swing.UIManager.LookAndFeelInfo info :
+    // javax.swing.UIManager.getInstalledLookAndFeels()) {
+    // if ("Nimbus".equals(info.getName())) {
+    // javax.swing.UIManager.setLookAndFeel(info.getClassName());
+    // break;
+    // }
+    // }
+    // } catch (ClassNotFoundException ex) {
+    // java.util.logging.Logger.getLogger(GroupingDemoFrame.class.getName()).log(java.util.logging.Level.SEVERE,
+    // null, ex);
+    // } catch (InstantiationException ex) {
+    // java.util.logging.Logger.getLogger(GroupingDemoFrame.class.getName()).log(java.util.logging.Level.SEVERE,
+    // null, ex);
+    // } catch (IllegalAccessException ex) {
+    // java.util.logging.Logger.getLogger(GroupingDemoFrame.class.getName()).log(java.util.logging.Level.SEVERE,
+    // null, ex);
+    // } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+    // java.util.logging.Logger.getLogger(GroupingDemoFrame.class.getName()).log(java.util.logging.Level.SEVERE,
+    // null, ex);
+    // }
+    // // </editor-fold>
+    //
+    // /* Create and display the form */
+    // java.awt.EventQueue.invokeLater(() -> {
+    // new GroupingDemoFrame(null).setVisible(true);
+    // });
+    // }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup btGrpBankDetails;
-    private javax.swing.ButtonGroup btGrpFeatureToUse;
     private javax.swing.JButton btSubmit;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JRadioButton rdBtEducation;
-    private javax.swing.JRadioButton rdBtFeatureJdk8;
-    private javax.swing.JRadioButton rdBtFeaturePreJdk8;
     private javax.swing.JRadioButton rdBtJob;
     private javax.swing.JRadioButton rdBtMaritalStatus;
     private org.jdesktop.swingx.JXTreeTable treeTblBankDetails;
