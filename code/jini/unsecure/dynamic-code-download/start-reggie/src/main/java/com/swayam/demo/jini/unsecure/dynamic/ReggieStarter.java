c979abff6e0f9f18c3a736f537eb8383cee9f141
paawak <paawak@gmail.com>
2015-03-01 17:01:16 -0500

    start jini with custom configuration

--- code/jini/unsecure/dynamic-code-download/start-reggie/src/main/java/com/swayam/demo/jini/unsecure/dynamic/ReggieStarter.java
+++ code/jini/unsecure/dynamic-code-download/start-reggie/src/main/java/com/swayam/demo/jini/unsecure/dynamic/ReggieStarter.java
@@ -1,16 +1,14 @@
 package com.swayam.demo.jini.unsecure.dynamic;
 
-import net.jini.config.Configuration;
 import net.jini.config.ConfigurationException;
-import net.jini.config.ConfigurationProvider;
 
 import com.sun.jini.start.ServiceStarter;
 
 public class ReggieStarter {
+
     public static void main(String[] args) throws ConfigurationException {
-        Configuration config = ConfigurationProvider
-                .getInstance(new String[] {
-                        "/kaaj/blog/code/jini/unsecure/jini-services/config/start-reggie.config" });
-        ServiceStarter.main(config);
+        System.setProperty("java.security.policy", System.getProperty("user.home") + "/jini/policy.all");
+        ServiceStarter.main(new ReggieStarterConfiguration());
     }
+
 }
---
+++ code/jini/unsecure/dynamic-code-download/start-reggie/src/main/java/com/swayam/demo/jini/unsecure/dynamic/ReggieStarterConfiguration.java
@@ -0,0 +1,53 @@
+package com.swayam.demo.jini.unsecure.dynamic;
+
+import net.jini.config.AbstractConfiguration;
+import net.jini.config.ConfigurationException;
+import net.jini.security.BasicProxyPreparer;
+import net.jini.security.ProxyPreparer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.jini.start.NonActivatableServiceDescriptor;
+import com.sun.jini.start.ServiceDescriptor;
+
+public class ReggieStarterConfiguration extends AbstractConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReggieStarterConfiguration.class);
+
+    private static final String COMPONENT_NAME = "com.sun.jini.start";
+
+    @Override
+    protected Object getEntryInternal(String component, String name, @SuppressWarnings("rawtypes") Class type,
+            Object data) throws ConfigurationException {
+
+        LOGGER.info("component:{}, name:{}, type:{}, data:{}", new Object[] { component, name, type, data });
+
+        checkConfiguration(component);
+
+        if (type == ServiceDescriptor[].class) {
+            return new ServiceDescriptor[] { getServiceDescriptor() };
+        } else if (type == ProxyPreparer.class) {
+            return new BasicProxyPreparer();
+        }
+
+        return null;
+    }
+
+    private void checkConfiguration(String component) {
+        if (!COMPONENT_NAME.equals(component)) {
+            throw new IllegalArgumentException("The configuration from " + component + " is not supported");
+        }
+    }
+
+    private ServiceDescriptor getServiceDescriptor() {
+        return new NonActivatableServiceDescriptor(
+                "",// codebase
+                "", // policy
+                "",// classpath
+                "com.sun.jini.reggie.TransientRegistrarImpl",
+                new String[] { "/kaaj/blog/code/jini/unsecure/jini-services/config/jeri-reggie.config" }// config
+        );
+    }
+
+}
