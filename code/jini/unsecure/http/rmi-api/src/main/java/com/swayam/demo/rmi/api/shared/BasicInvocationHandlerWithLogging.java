e666a1292695b5c96916bd6a0b315dd389786779
paawak <paawak@gmail.com>
2015-03-14 21:39:28 -0400

    fixed

--- code/jini/unsecure/http/rmi-api/src/main/java/com/swayam/demo/rmi/api/shared/BasicInvocationHandlerWithLogging.java
+++ code/jini/unsecure/http/rmi-api/src/main/java/com/swayam/demo/rmi/api/shared/BasicInvocationHandlerWithLogging.java
@@ -1,7 +1,5 @@
 package com.swayam.demo.rmi.api.shared;
 
-import java.io.Serializable;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 import net.jini.core.constraint.MethodConstraints;
@@ -11,24 +9,20 @@ import net.jini.jeri.ObjectEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BasicInvocationHandlerWithLogging implements InvocationHandler, Serializable {
+public class BasicInvocationHandlerWithLogging extends BasicInvocationHandler {
 
     private static final long serialVersionUID = 1L;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BasicInvocationHandlerWithLogging.class);
 
-    private final ObjectEndpoint oe;
-    private final MethodConstraints serverConstraints;
-
     public BasicInvocationHandlerWithLogging(ObjectEndpoint oe, MethodConstraints serverConstraints) {
-        this.oe = oe;
-        this.serverConstraints = serverConstraints;
+        super(oe, serverConstraints);
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         LOGGER.info("************** ");
-        return new BasicInvocationHandler(oe, serverConstraints).invoke(proxy, method, args);
+        return super.invoke(proxy, method, args);
     }
 
 }
--- code/jini/unsecure/http/rmi-server/src/main/java/com/swayam/demo/rmi/server/core/rmi/BasicILFactoryWithLogging.java
+++ code/jini/unsecure/http/rmi-server/src/main/java/com/swayam/demo/rmi/server/core/rmi/BasicILFactoryWithLogging.java
@@ -16,22 +16,11 @@ public class BasicILFactoryWithLogging extends BasicILFactory {
 
     @Override
     protected InvocationDispatcher createInvocationDispatcher(Collection methods, Remote impl, ServerCapabilities caps) throws ExportException {
-        if (impl == null) {
-            throw new NullPointerException("impl is null");
-        }
         return new BasicInvocationDispatcherWithLogging(methods, caps, null, null, getClassLoader());
     }
 
     @Override
     protected InvocationHandler createInvocationHandler(Class[] interfaces, Remote impl, ObjectEndpoint oe) throws ExportException {
-        for (int i = interfaces.length; --i >= 0;) {
-            if (interfaces[i] == null) {
-                throw new NullPointerException();
-            }
-        }
-        if (impl == null) {
-            throw new NullPointerException();
-        }
         return new BasicInvocationHandlerWithLogging(oe, null);
     }
 
