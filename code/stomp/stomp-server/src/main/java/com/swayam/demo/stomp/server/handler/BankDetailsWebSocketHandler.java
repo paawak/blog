e910ab2acd7f3c6c5c68fa1cf5a98a26c311f9de
Palash Ray <paawak@gmail.com>
2015-10-20 15:24:54 +0530

    a

--- code/stomp/stomp-server/src/main/java/com/swayam/demo/stomp/server/handler/BankDetailsWebSocketHandler.java
+++ code/stomp/stomp-server/src/main/java/com/swayam/demo/stomp/server/handler/BankDetailsWebSocketHandler.java
@@ -11,8 +11,7 @@ import com.swayam.demo.stomp.server.service.BankDetailService;
 
 public class BankDetailsWebSocketHandler extends TextWebSocketHandler {
 
-    private static final Logger LOGGER = LoggerFactory
-	    .getLogger(BankDetailsWebSocketHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BankDetailsWebSocketHandler.class);
 
     private final BankDetailService bankDetailService;
 
@@ -21,13 +20,10 @@ public class BankDetailsWebSocketHandler extends TextWebSocketHandler {
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session,
-	    TextMessage message) {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
 	LOGGER.info("recieved message from client");
-	BankDetailSortOrder group = BankDetailSortOrder.valueOf(message
-		.getPayload().toUpperCase());
-	bankDetailService.getBankDetailsAsync(group,
-		new DataListenerForStompHandlerImpl(session));
+	BankDetailSortOrder group = BankDetailSortOrder.valueOf(message.getPayload().toUpperCase());
+	bankDetailService.getBankDetailsAsync(group, new DataListenerForStompHandlerImpl(session));
     }
 
 }
