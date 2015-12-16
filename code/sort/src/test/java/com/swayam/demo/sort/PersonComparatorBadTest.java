21bd90f6ede0df23a050f08372c724ee27e1039a
Palash Ray <paawak@gmail.com>
2015-12-15 22:39:00 -0500

    add pasing data set

--- code/sort/src/test/java/com/swayam/demo/sort/PersonComparatorBadTest.java
+++ code/sort/src/test/java/com/swayam/demo/sort/PersonComparatorBadTest.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class PersonComparatorBadTest {
@@ -16,15 +14,16 @@ public class PersonComparatorBadTest {
     // @Rule
     // public ExpectedException thrown = ExpectedException.none();
 
-    private final List<Person> persons = new ArrayList<>();
-
     @Test
-    public void testCompare() {
+    public void testCompare_fail() {
 	// thrown.expect(IllegalArgumentException.class);
 	// thrown.expectMessage("Comparison method violates its general
 	// contract!");
 
 	// I want the exception trace to be printed out
+
+	List<Person> persons = getFailingSet();
+
 	try {
 	    Collections.sort(persons, new PersonComparatorBad());
 	    fail("Expecting a " + IllegalArgumentException.class);
@@ -35,8 +34,52 @@ public class PersonComparatorBadTest {
 
     }
 
-    @Before
-    public void setup() {
+    @Test
+    public void testCompare_pass() {
+	List<Person> persons = getPassingSet();
+	Collections.sort(persons, new PersonComparatorBad());
+	printPersons(persons);
+    }
+
+    private void printPersons(List<Person> persons) {
+	persons.forEach((Person person) -> {
+	    System.out.println(person.getId());
+	});
+    }
+
+    private List<Person> getPassingSet() {
+	List<Person> persons = new ArrayList<>();
+	persons.add(new Person("TestName", 3));
+	persons.add(new Person("TestName", 290));
+	persons.add(new Person("TestName", 1));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", null));
+	persons.add(new Person("TestName", 1));
+	persons.add(new Person("TestName", 398));
+	persons.add(new Person("TestName", 46));
+	persons.add(new Person("TestName", 45));
+	persons.add(new Person("TestName", 0));
+	persons.add(new Person("TestName", 3));
+	persons.add(new Person("TestName", 45));
+	persons.add(new Person("TestName", 130));
+	persons.add(new Person("TestName", 33));
+	persons.add(new Person("TestName", 56));
+	return persons;
+    }
+
+    private List<Person> getFailingSet() {
+	List<Person> persons = new ArrayList<>();
 	persons.add(new Person("TestName", 3));
 	persons.add(new Person("TestName", 290));
 	persons.add(new Person("TestName", 1));
@@ -69,11 +112,7 @@ public class PersonComparatorBadTest {
 	persons.add(new Person("TestName", 130));
 	persons.add(new Person("TestName", 33));
 	persons.add(new Person("TestName", 56));
-    }
-
-    @After
-    public void cleanup() {
-	persons.clear();
+	return persons;
     }
 
 }
