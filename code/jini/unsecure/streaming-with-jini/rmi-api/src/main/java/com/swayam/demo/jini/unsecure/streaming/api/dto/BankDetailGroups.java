afd16e25d22feb22edead80352cd9fd5f7e9ce1a
Palash Ray <paawak@gmail.com>
2016-01-01 18:58:58 -0500

    initial

---
+++ code/jini/unsecure/streaming-with-jini/rmi-api/src/main/java/com/swayam/demo/jini/unsecure/streaming/api/dto/BankDetailGroups.java
@@ -0,0 +1,7 @@
+package com.swayam.demo.jini.unsecure.streaming.api.dto;
+
+public enum BankDetailGroups {
+
+	JOB, MARITAL_STATUS, EDUCATION;
+
+}
---
+++ code/jini/unsecure/streaming-with-jini/rmi-api/src/main/java/com/swayam/demo/jini/unsecure/streaming/api/service/BankDetailService.java
@@ -0,0 +1,15 @@
+package com.swayam.demo.jini.unsecure.streaming.api.service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
+
+import com.swayam.demo.jini.unsecure.streaming.api.dto.BankDetail;
+import com.swayam.demo.jini.unsecure.streaming.api.dto.BankDetailGroups;
+
+public interface BankDetailService extends Remote {
+
+    Map<String, List<BankDetail>> getBankDetails(BankDetailGroups group) throws RemoteException;
+
+}
