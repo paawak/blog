f03022f78b7545c88ff00bfa077bccc75de63ad2
Palash Ray <paawak@gmail.com>
2016-01-01 19:04:19 -0500

    wait for reggie to start

--- code/jini/unsecure/streaming-with-jini/rmi-server/src/main/java/com/swayam/demo/jini/unsecure/streaming/server/core/SpringNonSecureRmiServer.java
+++ code/jini/unsecure/streaming-with-jini/rmi-server/src/main/java/com/swayam/demo/jini/unsecure/streaming/server/core/SpringNonSecureRmiServer.java
@@ -1,7 +1,6 @@
 package com.swayam.demo.jini.unsecure.streaming.server.core;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,13 +46,13 @@ public class SpringNonSecureRmiServer {
 	    ServiceStarter.main(new ReggieStarterConfiguration(SpringNonSecureRmiServer.class.getResource("/jeri-reggie.config")));
 
 	    try {
-		Thread.sleep(10_000);
+		Thread.sleep(1_000);
 	    } catch (InterruptedException e) {
 		LOG.error("Reggie was interrupted while starting up", e);
 		throw new RuntimeException(e);
 	    }
 
-	    LOG.info("Started Reggie successfully");
+	    LOG.info("***************** Started Reggie successfully");
 
 	    signalToStartServerThread.countDown();
 
@@ -73,14 +72,14 @@ public class SpringNonSecureRmiServer {
 	public void run() {
 
 	    try {
-		signalToStartServerThread.await(3, TimeUnit.SECONDS);
+		signalToStartServerThread.await();
 	    } catch (InterruptedException e) {
 		LOG.error("RMI Server was interrupted while waiting for Reggie to start", e);
 		throw new RuntimeException(e);
 	    }
 
 	    try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("server-application.xml")) {
-		LOG.info("The RMIServer is ready");
+		LOG.info("*********************The RMIServer is ready");
 	    }
 	}
 
