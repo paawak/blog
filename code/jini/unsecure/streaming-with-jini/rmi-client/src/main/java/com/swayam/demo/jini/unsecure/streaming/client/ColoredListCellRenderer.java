35aa9899e3d85307d70cfb6f01c40bc7f9599f0b
Palash Ray <paawak@gmail.com>
2016-01-03 20:09:19 -0500

    bug fix: on selection, list item should be highlighted

--- code/jini/unsecure/streaming-with-jini/rmi-client/src/main/java/com/swayam/demo/jini/unsecure/streaming/client/ColoredListCellRenderer.java
+++ code/jini/unsecure/streaming-with-jini/rmi-client/src/main/java/com/swayam/demo/jini/unsecure/streaming/client/ColoredListCellRenderer.java
@@ -1,5 +1,6 @@
 package com.swayam.demo.jini.unsecure.streaming.client;
 
+import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.JLabel;
@@ -12,31 +13,39 @@ public class ColoredListCellRenderer extends JLabel implements ListCellRenderer<
 
     private static final long serialVersionUID = 1L;
 
-    private static final String[] COLORS = { "red", "#6A5ACD", "#8B0000", "blue", "#B8860B", "#006400", "#FF1493", "#7FFF00", "#800080", "#A0522D" };
+    private static final String[] COLORS = { "#FF0000", "#6A5ACD", "#8B0000", "#0000FF", "#B8860B", "#006400", "#FF1493", "#7FFF00", "#800080", "#A0522D" };
 
     @Override
     public Component getListCellRendererComponent(JList<? extends BankDetail> list, BankDetail value, int index, boolean isSelected, boolean cellHasFocus) {
+
+	String foregroundColorHexCode;
+
 	if (isSelected) {
-	    setBackground(list.getSelectionBackground());
-	    setForeground(list.getSelectionForeground());
+	    String colorString = getColor(value);
+	    Integer colorCode = Integer.parseInt(colorString.substring(1), 16);
+	    Color bgColor = new Color(colorCode);
+	    setBackground(bgColor);
+	    foregroundColorHexCode = "#ffffff";
 	} else {
 	    setBackground(list.getBackground());
-	    setForeground(list.getForeground());
+	    foregroundColorHexCode = getColor(value);
 	}
 
 	setFont(list.getFont());
 	setOpaque(true);
 
-	setText(getHtmlWrappedText(value));
+	setText(getHtmlWrappedText(value, foregroundColorHexCode));
 
 	return this;
     }
 
-    private String getHtmlWrappedText(BankDetail value) {
-	StringBuilder sb = new StringBuilder(300);
+    private String getHtmlWrappedText(BankDetail value, String foregroundColorHexCode) {
+	StringBuilder sb = new StringBuilder(100);
 	sb.append("<html>");
-	sb.append("<p style=\"color: " + getColor(value) + ";\">");
-	sb.append(value.toString());
+	sb.append("<p style=\"color: ");
+	sb.append(foregroundColorHexCode);
+	sb.append(";\">");
+	sb.append(value);
 	sb.append("</p>");
 	sb.append("</html>");
 
