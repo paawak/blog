2a6bc64af5e40f5edee967bb20ea302302ff47e9
Palash Ray <paawak@gmail.com>
2016-04-08 15:01:07 -0400

    move config class to rit package

--- code/stomp/stomp-server/src/main/java/com/swayam/demo/stomp/server/config/WebSocketHandlerConfig.java
+++ code/stomp/stomp-server/src/main/java/com/swayam/demo/stomp/server/config/WebSocketHandlerConfig.java
@@ -1,4 +1,4 @@
-package com.swayam.demo.stomp.server.handler;
+package com.swayam.demo.stomp.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.ExceptionWebSocketHandlerDecorator;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
+import com.swayam.demo.stomp.server.handler.BankDetailsWebSocketHandler;
 import com.swayam.demo.stomp.server.service.BankDetailService;
 
 @Configuration
