9edb132578aaa81b154ee5dfe4d940d82b84cc46
Palash Ray <paawak@gmail.com>
2016-07-06 07:21:05 -0400

    declated aspect

---
+++ code/spring-aop/aop-end-user/src/main/java/com/swayam/demo/spring/aop/enduser/logging/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.swayam.demo.spring.aop.enduser.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* org.springframework.jdbc.core.JdbcOperations.*(..))")
+    private void jdbcOperationsPointcut() {
+    }
+
+    @Around("jdbcOperationsPointcut()")
+    public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable {
+
+	LOGGER.info("I am here");
+	// start stopwatch
+	Object retVal = pjp.proceed();
+	// stop stopwatch
+	return retVal;
+    }
+
+}
--- code/spring-aop/aop-end-user/src/main/java/com/swayam/demo/spring/aop/enduser/service/BankDetailServiceImpl.java
+++ code/spring-aop/aop-end-user/src/main/java/com/swayam/demo/spring/aop/enduser/service/BankDetailServiceImpl.java
@@ -2,13 +2,18 @@ package com.swayam.demo.spring.aop.enduser.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.swayam.demo.spring.aop.enduser.dao.BankDetailDao;
 import com.swayam.demo.spring.aop.enduser.model.BankDetail;
 
+@Service
 public class BankDetailServiceImpl implements BankDetailService {
 
     private final BankDetailDao bankDetailDao;
 
+    @Autowired
     public BankDetailServiceImpl(BankDetailDao bankDetailDao) {
 	this.bankDetailDao = bankDetailDao;
     }
