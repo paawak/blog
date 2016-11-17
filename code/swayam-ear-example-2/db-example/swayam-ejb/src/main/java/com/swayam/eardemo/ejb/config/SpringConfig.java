f0f98a2e35ce9e0a192989cb4f3a815116d8a32f
Palash Ray <paawak@gmail.com>
2016-11-18 02:36:29 +0530

    slight improvement

--- code/swayam-ear-example-2/db-example/swayam-ejb/src/main/java/com/swayam/eardemo/ejb/config/SpringConfig.java
+++ code/swayam-ear-example-2/db-example/swayam-ejb/src/main/java/com/swayam/eardemo/ejb/config/SpringConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan({ "com.swayam.eardemo.ejb.dao", "com.swayam.eardemo.ejb.service" })
+@ComponentScan("com.swayam.eardemo.ejb")
 @PropertySource("classpath:jdbc.properties")
 public class SpringConfig {
 
