7426205ad2318190c8e5b1105cf33899a6ebc6ed
paawak <paawak@home-desktop-pony>
2017-09-03 19:09:32 +0530

    print movie names

--- code/big-data/spark-demo/src/main/java/com/swayam/demo/bigdata/spark/rating/MovieRatingCalculator.java
+++ code/big-data/spark-demo/src/main/java/com/swayam/demo/bigdata/spark/rating/MovieRatingCalculator.java
@@ -1,6 +1,7 @@
 package com.swayam.demo.bigdata.spark.rating;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.apache.spark.SparkConf;
@@ -37,15 +38,28 @@ public class MovieRatingCalculator {
 				return outputTuple;
 			});
 
+			JavaPairRDD<Integer, String> movieMaps = sparkContext.textFile(HDFS_INPUT_OUTPUT_DIRECTORY + "movies.dat")
+					.mapToPair((String singleMovieLine) -> {
+						String[] tokens = singleMovieLine.split("::");
+						return new Tuple2<Integer, String>(Integer.parseInt(tokens[0]), tokens[1]);
+					});
+
 			// find the top rated 10 movies
-			List<Tuple2<Integer, Double>> topTenMovies = averageMovieRatings
+			List<Tuple2<String, Double>> topTenMovies = averageMovieRatings
 					.mapToPair((Tuple2<Integer, Double> input) -> {
 						return input.swap();
 					}).sortByKey(false).mapToPair((Tuple2<Double, Integer> input) -> {
 						return input.swap();
-					}).take(10);
+					}).take(10).stream().map((Tuple2<Integer, Double> input) -> {
+						String movieName = movieMaps.lookup(input._1).get(0);
+						return new Tuple2<String, Double>(movieName, input._2);
+					}).collect(Collectors.toList());
 
-			System.out.println("*********** topTenMovies: " + topTenMovies);
+			System.out.println("**********  Top 10 Movies ***********");
+
+			topTenMovies.forEach((Tuple2<String, Double> movieRating) -> {
+				System.out.println(movieRating._1 + "\t:\t" + movieRating._2);
+			});
 
 		}
 	}
