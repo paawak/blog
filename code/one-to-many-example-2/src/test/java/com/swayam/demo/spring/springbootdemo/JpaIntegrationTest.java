07b781ddc625779b1322578b7283186da2a857a5
Palash Ray <paawak@gmail.com>
2017-09-19 20:21:40 +0530

    jpa integration test

---
+++ code/one-to-many-example-2/src/test/java/com/swayam/demo/spring/springbootdemo/JpaIntegrationTest.java
@@ -0,0 +1,120 @@
+package com.swayam.demo.spring.springbootdemo;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
+import org.springframework.util.StringUtils;
+
+import com.swayam.demo.jpa.one2many.model.Author;
+import com.swayam.demo.jpa.one2many.model.Book;
+import com.swayam.demo.jpa.one2many.model.Chapter;
+import com.swayam.demo.jpa.one2many.model.Genre;
+import com.swayam.demo.jpa.one2many.model.Section;
+
+public class JpaIntegrationTest {
+
+	private EntityManager entityManager;
+
+	@Before
+	public void setup() {
+		String url = "jdbc:hsqldb:mem:test_db";
+		String user = "SA";
+		String password = "";
+
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+			Statement stat = con.createStatement();
+			stat.executeQuery(getLines("/sql/schema_hsql.sql"));
+			stat.executeQuery(getLines("/sql/data.sql"));
+			stat.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		entityManager = createEntityManager(url, user, password);
+	}
+
+	@After
+	public void cleanup() {
+		entityManager.close();
+		entityManager = null;
+	}
+
+	@Test
+	public void testBookQuery() {
+		Book book = entityManager.find(Book.class, 1L);
+		System.out.println(book);
+	}
+
+	private EntityManager createEntityManager(String url, String user, String password) {
+
+		MutablePersistenceUnitInfo mutablePersistenceUnitInfo = new MutablePersistenceUnitInfo() {
+			@Override
+			public ClassLoader getNewTempClassLoader() {
+				return null;
+			}
+		};
+
+		mutablePersistenceUnitInfo.setPersistenceUnitName("TestUnit");
+		mutablePersistenceUnitInfo.setPersistenceProviderClassName(HibernatePersistenceProvider.class.getName());
+
+		Properties props = new Properties();
+		props.put("hibernate.connection.url", url);
+		props.put("hibernate.connection.username", user);
+
+		if (StringUtils.hasText(password)) {
+			props.put("hibernate.connection.password", password);
+		}
+
+		mutablePersistenceUnitInfo.setProperties(props);
+
+		mutablePersistenceUnitInfo.addManagedClassName(Author.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Book.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Chapter.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Genre.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Section.class.getName());
+
+		PersistenceUnitDescriptor persistenceUnitDescriptor = new PersistenceUnitInfoDescriptor(
+				mutablePersistenceUnitInfo);
+
+		EntityManagerFactoryBuilder entityManagerFactoryBuilder = new EntityManagerFactoryBuilderImpl(
+				persistenceUnitDescriptor, Collections.EMPTY_MAP);
+
+		EntityManagerFactory entityManagerFactory = entityManagerFactoryBuilder.build();
+
+		return entityManagerFactory.createEntityManager();
+
+	}
+
+	private String getLines(String fileClasspath) {
+		List<String> lines;
+		try {
+			lines = Files.readAllLines(Paths.get(JpaIntegrationTest.class.getResource(fileClasspath).toURI()));
+		} catch (IOException | URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+
+		return StringUtils.collectionToDelimitedString(lines, "\n");
+	}
+
+}
