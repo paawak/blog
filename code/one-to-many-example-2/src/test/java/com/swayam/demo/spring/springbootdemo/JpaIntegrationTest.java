0f1329f9b4dcdad8e2f2c1447447200d460447a3
Palash Ray <paawak@gmail.com>
2017-10-30 23:16:01 +0530

    organised tests

--- code/one-to-many-example-2/src/test/java/com/swayam/demo/spring/springbootdemo/JpaIntegrationTest.java
+++ code/one-to-many-example-2/src/test/java/com/swayam/demo/spring/springbootdemo/JpaIntegrationTest.java
@@ -1,66 +1,17 @@
 package com.swayam.demo.spring.springbootdemo;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
-import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
-import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
-import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
-import org.springframework.util.StringUtils;
 
-import com.swayam.demo.jpa.one2many.model.Author;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swayam.demo.jpa.one2many.model.Book;
-import com.swayam.demo.jpa.one2many.model.Chapter;
-import com.swayam.demo.jpa.one2many.model.Genre;
-import com.swayam.demo.jpa.one2many.model.Section;
 
-public class JpaIntegrationTest {
-
-	private EntityManager entityManager;
-
-	@Before
-	public void setup() throws IOException {
-		Properties props = new Properties();
-		props.load(JpaIntegrationTest.class.getResourceAsStream("/application-test.properties"));
-		String dbUrl = props.getProperty("jdbc.url");
-		String dbUser = props.getProperty("jdbc.username");
-		String dbPassword = props.getProperty("jdbc.password");
-
-		try (Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
-			Statement stat = con.createStatement();
-			stat.executeQuery(getLines("/sql/schema_hsql.sql"));
-			stat.executeQuery(getLines("/sql/data_hsql.sql"));
-			stat.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-
-		entityManager = createEntityManager(dbUrl, dbUser, dbPassword);
-	}
-
-	@After
-	public void cleanup() {
-		entityManager.close();
-		entityManager = null;
-	}
+public class JpaIntegrationTest extends JpaIntegrationTestParent {
 
 	@Test
 	public void testBookQuery() {
@@ -68,55 +19,27 @@ public class JpaIntegrationTest {
 		System.out.println(book);
 	}
 
-	private EntityManager createEntityManager(String dbUrl, String dbUser, String dbPassword) {
+	@Test
+	public void testBookInsert() throws JsonParseException, JsonMappingException, IOException {
+		Book book = new ObjectMapper()
+				.readValue(JpaIntegrationTest.class.getResourceAsStream("/json/save_book_request_1.json"), Book.class);
 
-		MutablePersistenceUnitInfo mutablePersistenceUnitInfo = new MutablePersistenceUnitInfo() {
-			@Override
-			public ClassLoader getNewTempClassLoader() {
-				return null;
-			}
-		};
+		EntityTransaction entityTransaction = entityManager.getTransaction();
 
-		mutablePersistenceUnitInfo.setPersistenceUnitName("TestUnit");
-		mutablePersistenceUnitInfo.setPersistenceProviderClassName(HibernatePersistenceProvider.class.getName());
+		entityTransaction.begin();
 
-		Properties props = new Properties();
-		props.put("hibernate.connection.url", dbUrl);
-		props.put("hibernate.connection.username", dbUser);
+		entityManager.persist(book);
 
-		if (StringUtils.hasText(dbPassword)) {
-			props.put("hibernate.connection.password", dbPassword);
-		}
+		entityManager.flush();
 
-		mutablePersistenceUnitInfo.setProperties(props);
+		entityTransaction.commit();
+		entityManager.clear();
+		System.out.println(book);
 
-		mutablePersistenceUnitInfo.addManagedClassName(Author.class.getName());
-		mutablePersistenceUnitInfo.addManagedClassName(Book.class.getName());
-		mutablePersistenceUnitInfo.addManagedClassName(Chapter.class.getName());
-		mutablePersistenceUnitInfo.addManagedClassName(Genre.class.getName());
-		mutablePersistenceUnitInfo.addManagedClassName(Section.class.getName());
+		System.out.println("*****************************************");
 
-		PersistenceUnitDescriptor persistenceUnitDescriptor = new PersistenceUnitInfoDescriptor(
-				mutablePersistenceUnitInfo);
-
-		EntityManagerFactoryBuilder entityManagerFactoryBuilder = new EntityManagerFactoryBuilderImpl(
-				persistenceUnitDescriptor, Collections.EMPTY_MAP);
-
-		EntityManagerFactory entityManagerFactory = entityManagerFactoryBuilder.build();
-
-		return entityManagerFactory.createEntityManager();
-
-	}
-
-	private String getLines(String fileClasspath) {
-		List<String> lines;
-		try {
-			lines = Files.readAllLines(Paths.get(JpaIntegrationTest.class.getResource(fileClasspath).toURI()));
-		} catch (IOException | URISyntaxException e) {
-			throw new RuntimeException(e);
-		}
-
-		return StringUtils.collectionToDelimitedString(lines, "\n");
+		Book savedBook = entityManager.find(Book.class, book.getId());
+		System.out.println(savedBook);
 	}
 
 }
---
+++ code/one-to-many-example-2/src/test/java/com/swayam/demo/spring/springbootdemo/JpaIntegrationTestParent.java
@@ -0,0 +1,115 @@
+package com.swayam.demo.spring.springbootdemo;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
+import org.springframework.util.StringUtils;
+
+import com.swayam.demo.jpa.one2many.model.Author;
+import com.swayam.demo.jpa.one2many.model.Book;
+import com.swayam.demo.jpa.one2many.model.Chapter;
+import com.swayam.demo.jpa.one2many.model.Genre;
+import com.swayam.demo.jpa.one2many.model.Section;
+
+public abstract class JpaIntegrationTestParent {
+
+	protected EntityManager entityManager;
+
+	@Before
+	public void setup() throws IOException {
+		Properties props = new Properties();
+		props.load(JpaIntegrationTestParent.class.getResourceAsStream("/application-test.properties"));
+		String dbUrl = props.getProperty("jdbc.url");
+		String dbUser = props.getProperty("jdbc.username");
+		String dbPassword = props.getProperty("jdbc.password");
+
+		try (Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+			Statement stat = con.createStatement();
+			stat.executeQuery(getLines("/sql/schema_hsql.sql"));
+			stat.executeQuery(getLines("/sql/data_hsql.sql"));
+			stat.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		entityManager = createEntityManager(dbUrl, dbUser, dbPassword);
+	}
+
+	@After
+	public void cleanup() {
+		entityManager.close();
+		entityManager = null;
+	}
+
+	private EntityManager createEntityManager(String dbUrl, String dbUser, String dbPassword) {
+
+		MutablePersistenceUnitInfo mutablePersistenceUnitInfo = new MutablePersistenceUnitInfo() {
+			@Override
+			public ClassLoader getNewTempClassLoader() {
+				return null;
+			}
+		};
+
+		mutablePersistenceUnitInfo.setPersistenceUnitName("TestUnit");
+		mutablePersistenceUnitInfo.setPersistenceProviderClassName(HibernatePersistenceProvider.class.getName());
+
+		Properties props = new Properties();
+		props.put("hibernate.connection.url", dbUrl);
+		props.put("hibernate.connection.username", dbUser);
+
+		if (StringUtils.hasText(dbPassword)) {
+			props.put("hibernate.connection.password", dbPassword);
+		}
+
+		mutablePersistenceUnitInfo.setProperties(props);
+
+		mutablePersistenceUnitInfo.addManagedClassName(Author.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Book.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Chapter.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Genre.class.getName());
+		mutablePersistenceUnitInfo.addManagedClassName(Section.class.getName());
+
+		PersistenceUnitDescriptor persistenceUnitDescriptor = new PersistenceUnitInfoDescriptor(
+				mutablePersistenceUnitInfo);
+
+		EntityManagerFactoryBuilder entityManagerFactoryBuilder = new EntityManagerFactoryBuilderImpl(
+				persistenceUnitDescriptor, Collections.EMPTY_MAP);
+
+		EntityManagerFactory entityManagerFactory = entityManagerFactoryBuilder.build();
+
+		return entityManagerFactory.createEntityManager();
+
+	}
+
+	private String getLines(String fileClasspath) {
+		List<String> lines;
+		try {
+			lines = Files.readAllLines(Paths.get(JpaIntegrationTest.class.getResource(fileClasspath).toURI()));
+		} catch (IOException | URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+
+		return StringUtils.collectionToDelimitedString(lines, "\n");
+	}
+
+}
