f4419b4caff6b7c8938780e93e3d8a13aff367d6
Palash Ray <paawak@gmail.com>
2018-05-01 23:07:36 +0530

    create a runner

---
+++ code/algos-jmh/src/main/java/com/swayam/jmh/AlgosRunner.java
@@ -0,0 +1,20 @@
+package com.swayam.jmh;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import com.swayam.jmh.algos.IntegerArrayMatcherSimpleStringImplBenchmark;
+import com.swayam.jmh.algos.IntegerArrayMatcherTrieImplBenchmark;
+
+public class AlgosRunner {
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(IntegerArrayMatcherSimpleStringImplBenchmark.class.getSimpleName())
+				.include(IntegerArrayMatcherTrieImplBenchmark.class.getSimpleName()).warmupIterations(5)
+				.measurementIterations(5).forks(1).build();
+
+		new Runner(opt).run();
+	}
+}
--- code/algos-jmh/src/main/java/com/swayam/jmh/algos/IntegerArrayMatcherSimpleStringImplBenchmark.java
+++ code/algos-jmh/src/main/java/com/swayam/jmh/algos/IntegerArrayMatcherSimpleStringImplBenchmark.java
@@ -6,10 +6,6 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import com.swayam.practice.algos.strings.IntegerArrayMatcher;
 import com.swayam.practice.algos.strings.IntegerArrayMatcherSimpleStringImpl;
@@ -53,13 +49,6 @@ public class IntegerArrayMatcherSimpleStringImplBenchmark {
 		// when
 		@SuppressWarnings("unused")
 		boolean result = testClass.contains(9);
-	}
-
-	public static void main(String[] args) throws RunnerException {
-		Options opt = new OptionsBuilder().include(IntegerArrayMatcherSimpleStringImplBenchmark.class.getSimpleName())
-				.warmupIterations(5).measurementIterations(5).forks(1).build();
-
-		new Runner(opt).run();
 	}
 
 }
