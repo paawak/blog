339788c6332e4e5e60fbfe8e231024c23920f3a0
Palash Ray <paawak@gmail.com>
2018-07-13 22:28:39 +0530

    flyway postgres

---
+++ code/transaction-demo/src/main/sql/postgres/V2__author_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (1,'Balaichand','Mukhopadhyay');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (2,'Salman','Rushdie');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (3,'PG','Wodehouse');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (4,'Frederick','Forsyth');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (5,'Saradindu','Bandyopadhyay');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (6,'Rabindranath','Tagore');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (7,'Bankim','Chatterjee');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (8,'Sharat','Chatterjee');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (9,'Amitava','Ghosh');
+INSERT INTO AUTHOR (id,first_name,last_name) VALUES (10,'Arthur Conan','Doyle');
+
+
