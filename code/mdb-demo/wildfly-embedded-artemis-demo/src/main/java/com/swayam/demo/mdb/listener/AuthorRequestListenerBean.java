9117599b2836362537b97238466849e24c38969d
Palash Ray <paawak@gmail.com>
2018-07-18 19:05:35 +0530

    working example of a mdb with wildfly 13

---
+++ code/mdb-demo/wildfly-embedded-artemis-demo/src/main/java/com/swayam/demo/mdb/listener/AuthorRequestListenerBean.java
@@ -0,0 +1,37 @@
+package com.swayam.demo.mdb.listener;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@MessageDriven(activationConfig = { @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+	@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/queue/mytest") })
+public class AuthorRequestListenerBean implements MessageListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorRequestListenerBean.class);
+
+    @Override
+    public void onMessage(Message message) {
+
+	if (!(message instanceof TextMessage)) {
+	    throw new UnsupportedOperationException("Expecting a " + TextMessage.class);
+	}
+
+	if (message instanceof TextMessage) {
+	    TextMessage textMessage = (TextMessage) message;
+	    try {
+		LOGGER.info("Text message received: {}", textMessage.getText());
+	    } catch (JMSException e) {
+		LOGGER.error("exception reading messsage", e);
+	    }
+	}
+
+    }
+
+}
