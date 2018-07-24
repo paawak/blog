6320d3189d015511de4248da87cd3cb837345353
Palash Ray <paawak@gmail.com>
2018-07-24 23:45:53 +0530

    removed topic

--- code/mdb-demo/wildfly-external-activemq-demo/src/main/java/com/swayam/demo/mdb/web/servlet/HelloWorldMDBServletClient.java
+++ code/mdb-demo/wildfly-external-activemq-demo/src/main/java/com/swayam/demo/mdb/web/servlet/HelloWorldMDBServletClient.java
@@ -21,42 +21,18 @@ import java.io.PrintWriter;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
-import javax.jms.Destination;
 import javax.jms.JMSContext;
 import javax.jms.JMSDestinationDefinition;
 import javax.jms.JMSDestinationDefinitions;
 import javax.jms.Queue;
-import javax.jms.Topic;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Definition of the two JMS destinations used by the quickstart (one queue and
- * one topic).
- */
 @JMSDestinationDefinitions(value = {
-		@JMSDestinationDefinition(name = "java:/queue/HELLOWORLDMDBQueue", interfaceName = "javax.jms.Queue", destinationName = "HelloWorldMDBQueue"),
-		@JMSDestinationDefinition(name = "java:/topic/HELLOWORLDMDBTopic", interfaceName = "javax.jms.Topic", destinationName = "HelloWorldMDBTopic") })
-
-/**
- * <p>
- * A simple servlet 3 as client that sends several messages to a queue or a
- * topic.
- * </p>
- *
- * <p>
- * The servlet is registered and mapped to /HelloWorldMDBServletClient using the
- * {@linkplain WebServlet
- * 
- * @HttpServlet}.
- *                </p>
- *
- * @author Serge Pagop (spagop@redhat.com)
- *
- */
+		@JMSDestinationDefinition(name = "java:/queue/HELLOWORLDMDBQueue", interfaceName = "javax.jms.Queue", destinationName = "HelloWorldMDBQueue") })
 @WebServlet("/rest/author")
 public class HelloWorldMDBServletClient extends HttpServlet {
 
@@ -70,9 +46,6 @@ public class HelloWorldMDBServletClient extends HttpServlet {
 	@Resource(lookup = "java:/queue/HELLOWORLDMDBQueue")
 	private Queue queue;
 
-	@Resource(lookup = "java:/topic/HELLOWORLDMDBTopic")
-	private Topic topic;
-
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
@@ -80,14 +53,12 @@ public class HelloWorldMDBServletClient extends HttpServlet {
 		out.write(
 				"<h1>Quickstart: Example demonstrates the use of <strong>JMS 2.0</strong> and <strong>EJB 3.2 Message-Driven Bean</strong> in JBoss EAP.</h1>");
 		try {
-			boolean useTopic = req.getParameterMap().keySet().contains("topic");
-			final Destination destination = useTopic ? topic : queue;
 
-			out.write("<p>Sending messages to <em>" + destination + "</em></p>");
+			out.write("<p>Sending messages to <em>" + queue + "</em></p>");
 			out.write("<h2>The following messages will be sent to the destination:</h2>");
 			for (int i = 0; i < MSG_COUNT; i++) {
 				String text = "This is message " + (i + 1);
-				context.createProducer().send(destination, text);
+				context.createProducer().send(queue, text);
 				out.write("Message (" + i + "): " + text + "</br>");
 			}
 			out.write(
