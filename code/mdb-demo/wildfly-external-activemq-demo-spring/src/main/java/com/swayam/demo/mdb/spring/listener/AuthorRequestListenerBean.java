7ac9af66379c127d6f758949e8d49d7d6826cd7b
Palash Ray <paawak@gmail.com>
2018-07-28 16:03:01 +0530

    added @ResourceAdapter to the mdb

--- code/mdb-demo/wildfly-external-activemq-demo-spring/src/main/java/com/swayam/demo/mdb/spring/listener/AuthorRequestListenerBean.java
+++ code/mdb-demo/wildfly-external-activemq-demo-spring/src/main/java/com/swayam/demo/mdb/spring/listener/AuthorRequestListenerBean.java
@@ -7,33 +7,35 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import org.jboss.ejb3.annotation.ResourceAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@MessageDriven(name = "HelloWorldQueueMDB",
-	activationConfig = { @ActivationConfigProperty(propertyName = "destination", propertyValue = "HELLOWORLDMDBQueue"),
+@ResourceAdapter("activemq-rar.rar")
+@MessageDriven(name = "HelloWorldQueueMDB", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "HELLOWORLDMDBQueue"),
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
 		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
 public class AuthorRequestListenerBean implements MessageListener {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorRequestListenerBean.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthorRequestListenerBean.class);
 
-    @Override
-    public void onMessage(Message message) {
+	@Override
+	public void onMessage(Message message) {
 
-	if (!(message instanceof TextMessage)) {
-	    throw new UnsupportedOperationException("Expecting a " + TextMessage.class);
+		if (!(message instanceof TextMessage)) {
+			throw new UnsupportedOperationException("Expecting a " + TextMessage.class);
+		}
+
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			try {
+				LOGGER.info("Text message received: {}", textMessage.getText());
+			} catch (JMSException e) {
+				LOGGER.error("exception reading messsage", e);
+			}
+		}
+
 	}
-
-	if (message instanceof TextMessage) {
-	    TextMessage textMessage = (TextMessage) message;
-	    try {
-		LOGGER.info("Text message received: {}", textMessage.getText());
-	    } catch (JMSException e) {
-		LOGGER.error("exception reading messsage", e);
-	    }
-	}
-
-    }
 
 }
