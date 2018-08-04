a3ae6aa13c17f9edf369879da01583de322a0208
Palash Ray <paawak@gmail.com>
2018-08-04 23:12:38 +0530

    MDB working

---
+++ code/transaction-demo/cmt-transaction-demo/cmt-transaction-spring/src/main/java/com/swayam/demo/trx/cmt/spring/mdb/HelloWorldQueueMDB.java
@@ -0,0 +1,39 @@
+package com.swayam.demo.trx.cmt.spring.mdb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@MessageDriven(name = "HelloWorldQueueMDB", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "HELLOWORLDMDBQueue"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class HelloWorldQueueMDB implements MessageListener {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldQueueMDB.class);
+
+	@Override
+	public void onMessage(Message message) {
+
+		if (!(message instanceof TextMessage)) {
+			throw new UnsupportedOperationException("Expecting a " + TextMessage.class);
+		}
+
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			try {
+				LOGGER.info("Text message received: {}", textMessage.getText());
+			} catch (JMSException e) {
+				LOGGER.error("exception reading messsage", e);
+			}
+		}
+
+	}
+
+}
