96387d31c326a40e417c79b2fa07078539c85850
Palash Ray <paawak@gmail.com>
2018-08-15 21:20:27 +0530

    running classes with main methods

---
+++ code/algos/src/test/java/com/swayam/practice/algos/search/binarysearch/HelpFedoHackerEarthTestRunner.java
@@ -0,0 +1,21 @@
+package com.swayam.practice.algos.search.binarysearch;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HelpFedoHackerEarthTestRunner {
+
+    private static final String TEST_FILE_PATH_PREFIX = "/com/swayam/practice/algos/search/binarysearch/HelpFedoHackerEarth_";
+    private static final int TEST_COUNT = 6;
+
+    public static void main(String[] args) throws IOException {
+
+	for (int count = 1; count <= TEST_COUNT; count++) {
+	    InputStream inputStream = HelpFedoHackerEarthTestRunner.class.getResourceAsStream(TEST_FILE_PATH_PREFIX + count + ".txt");
+	    System.setIn(inputStream);
+	    HelpFedoHackerEarth.main(null);
+	}
+
+    }
+
+}
