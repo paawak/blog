861504c043d215a30ef3a99d66b76413b1344878
Palash Ray <paawak@gmail.com>
2018-08-19 00:55:38 +0530

    wip

--- code/jvm-gc-logs-demo/collect-gc.sh
+++ code/jvm-gc-logs-demo/collect-gc.sh
@@ -1,10 +1,10 @@
-$JAVA_8_HOME/bin/java -cp target/algos-1.1.jar com.swayam.practice.algos.search.binarysearch.HelpFedoHackerEarthTestRunner	\
-	-XX:+UnlockDiagnosticVMOptions      \
+$JAVA_8_HOME/bin/java \
+-XX:+UnlockDiagnosticVMOptions      \
 -XX:+PrintCompilation -XX:+PrintInlining      \
 -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps      \
 -XX:+PrintGCApplicationStoppedTime      \
 -XX:+PrintGCApplicationConcurrentTime      \
 -XX:+PrintTenuringDistribution      \
 -XX:+PrintAdaptiveSizePolicy      \
--Xloggc:/kaaj/source/blog/docs/java-performance/algo-gc.log
-
+-Xloggc:/kaaj/source/blog/code/jvm-gc-logs-demo/logs/algo-gc.log	\
+-cp target/jvm-gc-logs-demo.jar com.swayam.demo.jvm.gclog.HeavyLiftingCalculator
