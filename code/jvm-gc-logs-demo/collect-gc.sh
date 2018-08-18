81932fe7339051be1a6b587ad02de860ff8dcb6a
Palash Ray <paawak@gmail.com>
2018-08-19 00:46:17 +0530

    heavy lifting calculator for generating gc logs

---
+++ code/jvm-gc-logs-demo/collect-gc.sh
@@ -0,0 +1,10 @@
+$JAVA_8_HOME/bin/java -cp target/algos-1.1.jar com.swayam.practice.algos.search.binarysearch.HelpFedoHackerEarthTestRunner	\
+	-XX:+UnlockDiagnosticVMOptions      \
+-XX:+PrintCompilation -XX:+PrintInlining      \
+-XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps      \
+-XX:+PrintGCApplicationStoppedTime      \
+-XX:+PrintGCApplicationConcurrentTime      \
+-XX:+PrintTenuringDistribution      \
+-XX:+PrintAdaptiveSizePolicy      \
+-Xloggc:/kaaj/source/blog/docs/java-performance/algo-gc.log
+
---
+++ code/jvm-gc-logs-demo/src/main/java/com/swayam/demo/jvm/gclog/HeavyLiftingCalculator.java
@@ -0,0 +1,69 @@
+package com.swayam.demo.jvm.gclog;
+
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * <p>
+ * A program that manipulates large numbers and does heavy arithmatic, intening
+ * to show the generation of GC logs by JVM.
+ * </p>
+ * 
+ * <p>
+ * Inspired from:<br>
+ * <a href=
+ * "https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/help-fredo/">Problem
+ * Details</a>
+ * </p>
+ * 
+ * 
+ * @author paawak
+ *
+ */
+public class HeavyLiftingCalculator {
+
+    private static final String TEST_FILE_PATH_PREFIX = "/com/swayam/demo/jvm/gclog/";
+    private static final int TEST_COUNT = 11;
+
+    public static void main(String[] args) {
+
+        for (int count = 1; count <= TEST_COUNT; count++) {
+            InputStream inputStream = HeavyLiftingCalculator.class.getResourceAsStream(TEST_FILE_PATH_PREFIX + count + ".txt");
+            doWork(inputStream);
+        }
+
+    }
+
+    private static void doWork(InputStream is) {
+        try (Scanner s = new Scanner(is);) {
+            int arrayLength = Integer.parseInt(s.nextLine());
+
+            String[] numbers = s.nextLine().split("\\s");
+            BigInteger initialGuess = new BigInteger(numbers[0]);
+
+            BigInteger product = Arrays.stream(numbers).parallel().map((String token) -> {
+                return new BigInteger(token);
+            }).reduce((BigInteger left, BigInteger right) -> {
+                return left.multiply(right);
+            }).get();
+
+            for (int i = 0; i < 10; i++) {
+                BigInteger nextGuess = findNRoot(initialGuess, product, arrayLength);
+                if (nextGuess.equals(initialGuess)) {
+                    break;
+                }
+                initialGuess = nextGuess;
+            }
+        }
+    }
+
+    private static BigInteger findNRoot(BigInteger initialGuess, BigInteger num, int power) {
+        BigInteger func = initialGuess.pow(power).subtract(num);
+        BigInteger derivative = initialGuess.multiply(initialGuess);
+        BigInteger nextGuess = initialGuess.subtract(func.divide(derivative));
+        return nextGuess;
+    }
+
+}
