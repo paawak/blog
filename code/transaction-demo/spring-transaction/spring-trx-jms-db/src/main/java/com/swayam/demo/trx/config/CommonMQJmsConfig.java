7da9f6bb8d7fa0dccf499f7d36f4a90a8156a6f2
Palash Ray <paawak@gmail.com>
2018-10-12 20:35:56 +0530

    cleanup

--- code/transaction-demo/spring-transaction/spring-trx-jms-db/src/main/java/com/swayam/demo/trx/config/CommonMQJmsConfig.java
+++ code/transaction-demo/spring-transaction/spring-trx-jms-db/src/main/java/com/swayam/demo/trx/config/CommonMQJmsConfig.java
@@ -8,14 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.connection.JmsTransactionManager;
 import org.springframework.jms.core.JmsOperations;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.jms.listener.MessageListenerContainer;
-import org.springframework.jms.support.destination.DestinationResolver;
-import org.springframework.jms.support.destination.DynamicDestinationResolver;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,20 +24,6 @@ public class CommonMQJmsConfig {
 
     @Autowired
     private Environment environment;
-
-    @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, DestinationResolver destinationResolver) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setDestinationResolver(destinationResolver);
-        factory.setConcurrency("3-10");
-        return factory;
-    }
-
-    @Bean
-    public DestinationResolver destinationResolver() {
-        return new DynamicDestinationResolver();
-    }
 
     @Bean
     public MessageListenerContainer defaultMessageListenerContainer(ConnectionFactory connectionFactory, MessageListener messageListener) {
