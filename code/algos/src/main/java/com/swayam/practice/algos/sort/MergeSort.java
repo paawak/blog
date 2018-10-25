08dd55493f4c0e168ba856e0ba92377f01d2a233
Palash Ray <paawak@gmail.com>
2018-10-25 18:18:19 +0530

    not working, complex 1

--- code/algos/src/main/java/com/swayam/practice/algos/sort/MergeSort.java
+++ code/algos/src/main/java/com/swayam/practice/algos/sort/MergeSort.java
@@ -1,22 +1,68 @@
 package com.swayam.practice.algos.sort;
 
+import java.util.Optional;
+import java.util.Stack;
+
 public class MergeSort {
 
+    private final Stack<Integer> splitStack = new Stack<>();
+
     public int[] sort(int[] input) {
-        split(input);
-        return null;
+	splitRecursive(new SubArray(0, input.length));
+	System.out.println(splitStack);
+	return null;
     }
 
-    private void split(int[] input) {
-        if (input.length > 1) {
-            int midPoint = input.length / 2;
-            int[] upper = new int[midPoint];
-            System.arraycopy(input, 0, upper, 0, midPoint);
-            split(upper);
-            int[] lower = new int[input.length - midPoint];
-            System.arraycopy(input, midPoint, lower, 0, input.length - midPoint);
-            split(lower);
-        }
+    private void splitRecursive(SubArray subArray) {
+	Node node = split(subArray);
+
+	if (node.lower.isPresent()) {
+	    splitRecursive(node.upper);
+	    splitRecursive(node.lower.get());
+	} else {
+
+	    if (node.upper.length == 1) {
+		splitStack.push(node.upper.startIndex);
+	    } else {
+		throw new IllegalArgumentException();
+	    }
+
+	}
+
+    }
+
+    Node split(SubArray subArray) {
+
+	if (subArray.length == 1) {
+	    return new Node(new SubArray(subArray.startIndex, 1), null);
+	}
+
+	int midPoint = subArray.length / 2;
+
+	SubArray upper = new SubArray(subArray.startIndex, midPoint);
+	SubArray lower = new SubArray(midPoint + subArray.startIndex, subArray.length - midPoint);
+
+	return new Node(upper, lower);
+    }
+
+    static class Node {
+	final SubArray upper;
+	final Optional<SubArray> lower;
+
+	Node(SubArray upper, SubArray lower) {
+	    this.upper = upper;
+	    this.lower = Optional.ofNullable(lower);
+	}
+    }
+
+    static class SubArray {
+	final int startIndex;
+	final int length;
+
+	SubArray(int startIndex, int length) {
+	    this.startIndex = startIndex;
+	    this.length = length;
+	}
     }
 
 }
