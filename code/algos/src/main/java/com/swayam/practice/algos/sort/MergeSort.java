b7fa9f249dc07f7f4bcfefd317f095df4847a1c1
Palash Ray <paawak@gmail.com>
2018-10-25 20:14:09 +0530

    bad attempt

--- code/algos/src/main/java/com/swayam/practice/algos/sort/MergeSort.java
+++ code/algos/src/main/java/com/swayam/practice/algos/sort/MergeSort.java
@@ -1,22 +1,79 @@
 package com.swayam.practice.algos.sort;
 
+import java.util.Stack;
+
 public class MergeSort {
 
+    private final Stack<Integer> splitstack = new Stack<>();
+
     public int[] sort(int[] input) {
-        split(input);
-        return null;
+
+	if (input.length < 2) {
+	    return input;
+	}
+
+	System.out.println("__________________");
+	split(input);
+	System.out.println(splitstack);
+
+	int[] merged = new int[] { splitstack.pop() };
+
+	while (!splitstack.empty()) {
+	    merged = merge(new int[] { splitstack.pop() }, merged);
+	}
+
+	return merged;
     }
 
     private void split(int[] input) {
-        if (input.length > 1) {
-            int midPoint = input.length / 2;
-            int[] upper = new int[midPoint];
-            System.arraycopy(input, 0, upper, 0, midPoint);
-            split(upper);
-            int[] lower = new int[input.length - midPoint];
-            System.arraycopy(input, midPoint, lower, 0, input.length - midPoint);
-            split(lower);
-        }
+	if (input.length == 1) {
+	    splitstack.push(input[0]);
+	} else {
+	    int midPoint = input.length / 2;
+	    int[] upper = new int[midPoint];
+	    System.arraycopy(input, 0, upper, 0, midPoint);
+	    split(upper);
+	    int[] lower = new int[input.length - midPoint];
+	    System.arraycopy(input, midPoint, lower, 0, input.length - midPoint);
+	    split(lower);
+	}
+    }
+
+    private int[] merge(int[] arr1, int[] arr2) {
+	int[] merged = new int[arr1.length + arr2.length];
+
+	for (int subArrayCtr = 0, mergedCtr = 0; subArrayCtr < merged.length; subArrayCtr++) {
+
+	    if (subArrayCtr < arr1.length && subArrayCtr < arr2.length) {
+
+		int ele1 = arr1[subArrayCtr];
+		int ele2 = arr2[subArrayCtr];
+
+		if (ele1 < ele2) {
+		    merged[mergedCtr] = ele1;
+		    merged[mergedCtr + 1] = ele2;
+		} else {
+		    merged[mergedCtr] = ele2;
+		    merged[mergedCtr + 1] = ele1;
+		}
+
+		mergedCtr += 2;
+
+	    } else if (subArrayCtr < arr1.length) {
+
+		merged[mergedCtr] = arr1[subArrayCtr];
+		mergedCtr++;
+
+	    } else if (subArrayCtr < arr2.length) {
+
+		merged[mergedCtr] = arr2[subArrayCtr];
+		mergedCtr++;
+
+	    }
+
+	}
+
+	return merged;
     }
 
 }
--- code/algos/src/test/java/com/swayam/practice/algos/sort/MergeSortTest.java
+++ code/algos/src/test/java/com/swayam/practice/algos/sort/MergeSortTest.java
@@ -1,31 +1,35 @@
 package com.swayam.practice.algos.sort;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Test;
 
 public class MergeSortTest {
 
     @Test
     public void testSort_even_length() {
-        // given
-        int[] input = new int[] { 40, 2, 54, 1, 8, 11, 95, 10 };
-        MergeSort testClass = new MergeSort();
+	// given
+	int[] input = new int[] { 40, 2, 54, 1, 8, 11, 95, 10 };
+	MergeSort testClass = new MergeSort();
 
-        // when
-        testClass.sort(input);
+	// when
+	int[] result = testClass.sort(input);
 
-        // then
+	// then
+	assertArrayEquals(new int[] { 1, 2, 8, 10, 11, 40, 54, 95 }, result);
     }
 
     @Test
     public void testSort_odd_length() {
-        // given
-        int[] input = new int[] { 40, 2, 54, 1, 8, 11, 95, 10, 57 };
-        MergeSort testClass = new MergeSort();
+	// given
+	int[] input = new int[] { 40, 2, 54, 1, 8, 11, 95, 10, 57 };
+	MergeSort testClass = new MergeSort();
 
-        // when
-        testClass.sort(input);
+	// when
+	int[] result = testClass.sort(input);
 
-        // then
+	// then
+	assertArrayEquals(new int[] { 1, 2, 8, 10, 11, 40, 54, 57, 95 }, result);
     }
 
 }
