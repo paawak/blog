62301ad4155a28f6b24cdb33d4760c7aebaaebb6
Palash Ray <paawak@gmail.com>
2018-10-28 13:47:39 +0530

    working, 1st cut

--- code/algos/src/main/java/com/swayam/practice/algos/sort/MergeSort.java
+++ code/algos/src/main/java/com/swayam/practice/algos/sort/MergeSort.java
@@ -8,72 +8,56 @@ public class MergeSort {
 
     public int[] sort(int[] input) {
 
-	if (input.length < 2) {
-	    return input;
-	}
+        if (input.length < 2) {
+            return input;
+        }
 
-	System.out.println("__________________");
-	split(input);
-	System.out.println(splitstack);
+        System.out.println("__________________");
+        split(input);
+        System.out.println(splitstack);
 
-	int[] merged = new int[] { splitstack.pop() };
+        int[] merged = new int[] { splitstack.pop() };
 
-	while (!splitstack.empty()) {
-	    merged = merge(new int[] { splitstack.pop() }, merged);
-	}
+        while (!splitstack.empty()) {
+            merged = merge(new int[] { splitstack.pop() }, merged);
+        }
 
-	return merged;
+        return merged;
     }
 
     private void split(int[] input) {
-	if (input.length == 1) {
-	    splitstack.push(input[0]);
-	} else {
-	    int midPoint = input.length / 2;
-	    int[] upper = new int[midPoint];
-	    System.arraycopy(input, 0, upper, 0, midPoint);
-	    split(upper);
-	    int[] lower = new int[input.length - midPoint];
-	    System.arraycopy(input, midPoint, lower, 0, input.length - midPoint);
-	    split(lower);
-	}
+        if (input.length == 1) {
+            splitstack.push(input[0]);
+        } else {
+            int midPoint = input.length / 2;
+            int[] upper = new int[midPoint];
+            System.arraycopy(input, 0, upper, 0, midPoint);
+            split(upper);
+            int[] lower = new int[input.length - midPoint];
+            System.arraycopy(input, midPoint, lower, 0, input.length - midPoint);
+            split(lower);
+        }
     }
 
     private int[] merge(int[] arr1, int[] arr2) {
-	int[] merged = new int[arr1.length + arr2.length];
+        int[] merged = new int[arr1.length + arr2.length];
 
-	for (int subArrayCtr = 0, mergedCtr = 0; subArrayCtr < merged.length; subArrayCtr++) {
+        for (int mergedCtr = 0, arr1Ctr = 0, arr2Ctr = 0; mergedCtr < merged.length; mergedCtr++) {
 
-	    if (subArrayCtr < arr1.length && subArrayCtr < arr2.length) {
+            if (arr1Ctr < arr1.length && arr2Ctr < arr2.length) {
+                if (arr1[arr1Ctr] < arr2[arr2Ctr]) {
+                    merged[mergedCtr] = arr1[arr1Ctr++];
+                } else {
+                    merged[mergedCtr] = arr2[arr2Ctr++];
+                }
+            } else if (arr1Ctr < arr1.length) {
+                merged[mergedCtr] = arr1[arr1Ctr++];
+            } else if (arr2Ctr < arr2.length) {
+                merged[mergedCtr] = arr2[arr2Ctr++];
+            }
+        }
 
-		int ele1 = arr1[subArrayCtr];
-		int ele2 = arr2[subArrayCtr];
-
-		if (ele1 < ele2) {
-		    merged[mergedCtr] = ele1;
-		    merged[mergedCtr + 1] = ele2;
-		} else {
-		    merged[mergedCtr] = ele2;
-		    merged[mergedCtr + 1] = ele1;
-		}
-
-		mergedCtr += 2;
-
-	    } else if (subArrayCtr < arr1.length) {
-
-		merged[mergedCtr] = arr1[subArrayCtr];
-		mergedCtr++;
-
-	    } else if (subArrayCtr < arr2.length) {
-
-		merged[mergedCtr] = arr2[subArrayCtr];
-		mergedCtr++;
-
-	    }
-
-	}
-
-	return merged;
+        return merged;
     }
 
 }
