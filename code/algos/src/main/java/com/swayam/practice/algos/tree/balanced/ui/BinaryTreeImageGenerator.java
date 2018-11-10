b908f26d7d77dac35e4dc0cef41eee221ee80e9b
Palash Ray <paawak@gmail.com>
2018-11-10 20:14:02 +0530

    binary tree ui

---
+++ code/algos/src/main/java/com/swayam/practice/algos/tree/balanced/ui/BinaryTreeImageGenerator.java
@@ -0,0 +1,15 @@
+package com.swayam.practice.algos.tree.balanced.ui;
+
+import java.awt.image.BufferedImage;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreeImageGenerator {
+
+    public BufferedImage getImage(int width, int height, TreeNode node) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        return image;
+    }
+
+}
