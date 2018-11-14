add41948afc5af416d5af2e52ccf7fe248b7aba0
Palash Ray <paawak@gmail.com>
2018-11-14 16:31:00 +0530

    optimised

--- code/algos/src/main/java/com/swayam/practice/algos/tree/bfs/TreeBFSWithoutQueue.java
+++ code/algos/src/main/java/com/swayam/practice/algos/tree/bfs/TreeBFSWithoutQueue.java
@@ -29,48 +29,45 @@ public class TreeBFSWithoutQueue {
 
     public void printSiblings(TreeNode root) {
 
-        int height = getHeight(root);
+	int height = getHeight(root);
 
-        for (int depth = 1; depth <= height; depth++) {
-            printSiblings(root, depth);
-            System.out.println();
-        }
+	for (int depth = 1; depth <= height; depth++) {
+	    printSiblings(root, depth);
+	    System.out.println();
+	}
 
     }
 
     private void printSiblings(TreeNode node, int depth) {
 
-        if (node == null) {
-            return;
-        }
+	if (node == null) {
+	    return;
+	}
 
-        if (depth == 1) {
-            System.out.print(node.getText() + "\t");
-        }
+	if (depth == 1) {
+	    System.out.print(node.getText() + "\t");
+	    return;
+	}
 
-        if (depth > 1) {
+	if (node.getLeft() != null) {
+	    printSiblings(node.getLeft(), depth - 1);
+	}
 
-            if (node.getLeft() != null) {
-                printSiblings(node.getLeft(), depth - 1);
-            }
-
-            if (node.getRight() != null) {
-                printSiblings(node.getRight(), depth - 1);
-            }
-
-        }
+	if (node.getRight() != null) {
+	    printSiblings(node.getRight(), depth - 1);
+	}
 
     }
 
     private int getHeight(TreeNode node) {
 
-        if (node == null) {
-            return 0;
-        } else if (node.getLeft() == null && node.getRight() == null) {
-            return 1;
-        } else {
-            return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
-        }
+	if (node == null) {
+	    return 0;
+	} else if (node.getLeft() == null && node.getRight() == null) {
+	    return 1;
+	} else {
+	    return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
+	}
 
     }
 
