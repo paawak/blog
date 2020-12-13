b5773cec3225484814ff5d0ef8340235a26b53d9
Palash Ray <paawak@gmail.com>
2020-12-13 20:29:01 +0530

    layout

--- code/php/library-ui/src/Author.tsx
+++ code/php/library-ui/src/Author.tsx
@@ -15,11 +15,47 @@ class Author extends Component<AuthorProps, AuthorState> {
                 <div className="container">
                     <div className="row align-items-start">
                         <div className="col form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                            <label htmlFor="firstName">First name</label>
+                            <input type="text" className="form-control" id="firstName" />
                         </div>
                     </div>
+                    <div className="row align-items-start">
+                        <div className="col form-group">
+                            <label htmlFor="lastName">Last name</label>
+                            <input type="text" className="form-control" id="lastName" />
+                        </div>
+                    </div>   
+                    <div className="row align-items-start">
+                        <div className="col form-group">
+                            <label htmlFor="address">Address</label>
+                            <input type="text" className="form-control" id="address" />
+                        </div>
+                    </div>
+                    <div className="row align-items-start">
+                        <div className="col form-group">
+                            <label htmlFor="city">City</label>
+                            <input type="text" className="form-control" id="city" />
+                        </div>
+                    </div>
+                    <div className="row align-items-start">
+                        <div className="col form-group">
+                            <label htmlFor="state">State</label>
+                            <input type="text" className="form-control" id="state" />
+                        </div>
+                    </div>
+                    <div className="row align-items-start">
+                        <div className="col form-group">
+                            <label htmlFor="zipCode">Zip code</label>
+                            <input type="text" className="form-control" id="zipCode" />
+                        </div>
+                    </div>    
+                    <div className="row align-items-start">
+                        <div className="col form-group">
+                            <label htmlFor="country">Country</label>
+                            <input type="text" className="form-control" id="country" />
+                        </div>
+                    </div>  
+                    <button type="button" className="btn btn-primary">Submit</button>                                                                                                                
                 </div>
             </div>
         );
