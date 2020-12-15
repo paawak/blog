5bd7076de00e3917eb989e601bbdae42c216c0c7
Palash Ray <paawak@gmail.com>
2020-12-15 10:59:18 +0530

    state

--- code/php/library-ui/src/Author.tsx
+++ code/php/library-ui/src/Author.tsx
@@ -15,15 +15,18 @@ interface AuthorState {
 
 class Author extends Component<AuthorProps, AuthorState> {
 
+    state: AuthorState = {
+        firstName: '',
+        lastName: '',
+        address: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        country: ''
+    };
+
     handleSubmit = () => {
         console.log('*****', this.state);
-    }
-
-    handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        console.log('*****', event.target.id, event.target.value);
-        this.setState({
-            firstName: event.target.value
-          });
     }
 
     render() {
@@ -34,43 +37,43 @@ class Author extends Component<AuthorProps, AuthorState> {
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="firstName">First name</label>
-                            <input type="text" className="form-control" id="firstName" onChange={this.handleChange} />
+                            <input type="text" className="form-control" id="firstName" value={this.state.firstName} onChange={(event)=>{this.setState({firstName: event.target.value});}} />
                         </div>
                     </div>
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="lastName">Last name</label>
-                            <input type="text" className="form-control" id="lastName" />
+                            <input type="text" className="form-control" id="lastName" value={this.state.lastName} onChange={(event)=>{this.setState({lastName: event.target.value});}} />
                         </div>
                     </div>   
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="address">Address</label>
-                            <input type="text" className="form-control" id="address" />
+                            <input type="text" className="form-control" id="address" value={this.state.address} onChange={(event)=>{this.setState({address: event.target.value});}} />
                         </div>
                     </div>
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="city">City</label>
-                            <input type="text" className="form-control" id="city" />
+                            <input type="text" className="form-control" id="city" value={this.state.city} onChange={(event)=>{this.setState({city: event.target.value});}} />
                         </div>
                     </div>
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="state">State</label>
-                            <input type="text" className="form-control" id="state" />
+                            <input type="text" className="form-control" id="state" value={this.state.state} onChange={(event)=>{this.setState({state: event.target.value});}} />
                         </div>
                     </div>
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="zipCode">Zip code</label>
-                            <input type="text" className="form-control" id="zipCode" />
+                            <input type="text" className="form-control" id="zipCode" value={this.state.zipCode} onChange={(event)=>{this.setState({zipCode: event.target.value});}} />
                         </div>
                     </div>    
                     <div className="row align-items-start">
                         <div className="col form-group">
                             <label htmlFor="country">Country</label>
-                            <input type="text" className="form-control" id="country" />
+                            <input type="text" className="form-control" id="country" value={this.state.country} onChange={(event)=>{this.setState({country: event.target.value});}} />
                         </div>
                     </div>  
                     <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>                                                                                                                
