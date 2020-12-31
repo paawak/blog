01822ab87bddf23104b44ceba00770928d73319f
Palash Ray <paawak@gmail.com>
2020-12-31 21:21:48 +0530

    adding nav bar

--- code/php/library-ui/src/App.tsx
+++ code/php/library-ui/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Author from './Author';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <Author />
+    <>
+      <NavBar />
+      <Author />
+    </>
   );
 }
 
