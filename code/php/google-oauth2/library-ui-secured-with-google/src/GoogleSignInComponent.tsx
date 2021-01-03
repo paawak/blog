1a7c1b79aa6140a8f3441325a809b6f053369cac
Palash Ray <paawak@gmail.com>
2021-01-03 18:47:26 +0530

    added sign in component

---
+++ code/php/google-oauth2/library-ui-secured-with-google/src/GoogleSignInComponent.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent, useState } from 'react'
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+import { Alert } from './Alert';
+import AlertType from './AlertType';
+
+const CLIENT_ID = '955630342713-55eu6b3k5hmsg8grojjmk8mj1gi47g37.apps.googleusercontent.com';
+
+interface GoogleSignInComponentProps {
+  loginSuccess: (response: GoogleLoginResponse | GoogleLoginResponseOffline) => void;
+}
+
+export const GoogleSignInComponent: FunctionComponent<GoogleSignInComponentProps> = ({ loginSuccess }) => {
+
+  const [loginFailed, setLoginFailed] = useState<boolean>();
+
+  return (
+    <div className="text-center mb-4">
+      <h1 className="h3 mb-3 font-weight-normal">Welcome to Library Portal</h1>
+      {loginFailed &&
+        <Alert message="Could not sign you in! Try again." type={AlertType.ERROR} />
+      }
+      <p>Sign In</p>
+      <GoogleLogin
+        clientId={CLIENT_ID}
+        buttonText='Google'
+        onSuccess={loginSuccess}
+        onFailure={(response: any) => {
+          setLoginFailed(true);
+        }}
+        cookiePolicy={'single_host_origin'}
+        responseType='code,token'
+      />
+    </div>
+  )
+};
